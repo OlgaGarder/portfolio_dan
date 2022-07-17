6fbf7b4be4c4c37f253bdd47a42528f8d0a4d59c
OlgaGarder <50359762>
2022-07-17 15:17:23 +0300

    init

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  output: {
+    publicPath: '/',
+    assetModuleFilename: 'img/[name][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    open: true,
+  }   
+});
