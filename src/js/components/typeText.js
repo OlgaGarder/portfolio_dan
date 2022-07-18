2bae61deca432453764be420bff808854ebd672b
OlgaGarder <50359762>
2022-07-18 19:07:05 +0300

    section about/sills/ aniamte

---
+++ src/js/components/typeText.js
@@ -0,0 +1,13 @@
+import TypeIt from "typeit";
+
+new TypeIt("#about", {
+    strings: "Обо мне",
+    speed: 145,
+    loop: true,
+}).go();
+
+new TypeIt("#skills", {
+    strings: "Мои навыки",
+    speed: 145,
+    loop: true,
+}).go();
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import '../scss/main.scss';
+
+import './components/scroll';
+import './components/typeText';
